5080a780439148c4e853856765d60bb2dfbf6f75
Ahmed saleh <salehdah@hotmail.com>
2022-08-17 21:02:57 -0400

    chapter 2 new pages

---
+++ src/git2/mentor.java
@@ -0,0 +1,14 @@
+package git2;
+
+import java.util.ArrayList;
+
+public class mentor {
+    public static void main(String[] args) {
+        ArrayList<String> cars=new ArrayList<>();
+        cars.add("volvo");
+        cars.add("bmw");
+        cars.add("ford");
+        cars.add("mazda");
+        System.out.println(cars.add("ferrari"));
+    }
+}
